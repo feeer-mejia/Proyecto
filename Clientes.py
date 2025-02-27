450f84afc9665374e520061d928b2f12288a5c1b
feeer-mejia <kevin.navarrete2026@hilasal.superete.org.sv>
2025-02-27 02:05:37 -0600

    Proyecto finish

--- Clientes.py
+++ Clientes.py
@@ -2,43 +2,42 @@
 Almacenamiento = {"nombres": [], "numeros": [], "direcciones": []}
 
 def MenuPrincipal():
-    print(f"Querido/a {name}, Nuestra lista de movimientos son los siguientes \n 1- Registro de datos \n 2- Búsqueda de clientes \n 3- Lista de clientes \n 4- extracción de cliente \n 5- salir \n ")
+    print(f"\nQuerido/a {name}, Nuestra lista de movimientos son los siguientes \n 1- Registro de datos \n 2- Búsqueda de datos \n 3- Listado de clientes \n 4- eliminción de datos  \n 5- salir \n ")
 
 def RetiradaDeEmergencia():
-    Decision = input("\n ¿Quieres regresar al menú principal? Responde con 'sí' o 'no' : \n").lower()
-    if Decision == "si":
+    BackMenu = input("\n¿Quieres regresar al menú principal? Responde con 'sí' o 'no' : \n").lower()
+    if BackMenu == "si":
         MenuPrincipal()
-    elif Decision == "no":
-        Decision1 = (input("\n ¿quieres salir del programa? Responde con 'si' o 'no' : \n"))
-        if Decision1 == "si":
-            print("\n Elige la opción 5 para salir \n")
-        elif Decision1 == "no":
-            DecisionN1 = (input("\n ¿Quieres volver al menu principal? Responde con 'si' o 'no' : \n"))
-            if DecisionN1 == "si":
+    elif BackMenu == "no":
+        Exit = (input("\n¿quieres salir del programa? Responde con 'si' o 'no' : \n"))
+        if Exit == "si":
+            print("\nElige la opción 5 para salir \n")
+        elif Exit == "no":
+            BackMenu1 = (input("\n¿Quieres volver al menu principal? Responde con 'si' o 'no' : \n"))
+            if BackMenu1 == "si":
                 MenuPrincipal()
             else:
-                print("\n No podemos realizar tu decisión, espero que comprenda, gracias \n")
-
+                print("\nNo podemos realizar tu decisión, espero que comprenda, gracias. \n")
 
 def registro():
     print("\n 1. Nombres \n 2. Numeros \n 3. Direcciones \n 4. Salir :")
-    Option2 = (int(input("Ingresa la categoría de registros : ")))
-    if Option2 == 1:
+    Registration = int(input("Ingresa la categoría de registros : "))
+    if Registration == 1:
         print("\n Tu elección es nombres ")
         Names = (input("Introduce el nombre que quieres registrar :"))
         Confirmation1 = (input("Escribe 'confirmo' para enviar el nombre : "))
         Con1 = Confirmation1.lower()
         if Con1 == "confirmo":
-            Almacenamiento["nombres"].append(Names)
-            return Names
-    elif Option2 == 2:
+            Almacenamiento["nombres"].append(Names) 
+            return None
+    elif Registration == 2:
         print("Tu elección es numeros")
-        Numbers = (int(input("Introduce el numero que quieres registrar : ")))
+        Numbers = int(input("Introduce el numero que quieres registrar : "))
         Confirmation2 = (input("Escribe 'confirmo' para enviar el numero :")).lower()
         if Confirmation2 == "confirmo":
             Almacenamiento["numeros"].append(Numbers)
-            return Numbers
-    elif Option2 == 3:
+            return None
+    elif Registration == 3:
         print("Tu elección es direcciones")
         Adress = (input("Introduce la dirección que quieres registrar : "))
         Confirmation3 = (input("Escribe 'confirmo' para enviar la dirección :"))
@@ -46,8 +45,8 @@ def registro():
         AdressSpace = Adress + " "
         if Con3 == "confirmo":
             Almacenamiento["direcciones"].append(AdressSpace)
-            return AdressSpace
-    elif Option2 == 4:
+            return None
+    elif Registration == 4:
         AgainMenu = (input("¿Volver al menu? responde con si o no : ")).lower()
         if AgainMenu == "si":
             return None
@@ -58,31 +57,31 @@ def busqueda():
     print("¿Qué quieres buscar?")
     Response = int((input("\n 1. Nombre \n 2. Numeros \n 3. Direcciones \n 4. Salir : ")))
     if Response == 1:
-        print("Tu elección es busqueda de nombres")
-        SearchNam = input("Introduce el nombre que deseas buscar : ")
-        if SearchNam in Almacenamiento["nombres"]:
-            NombreEncontrado = Almacenamiento["nombres"].index(SearchNam)
-            print(f"El nombre que ingresaste es {SearchNam} y la posición es {NombreEncontrado}")
+        print("Tu elección es busqueda de nombres \n")
+        SearchName = input("Introduce el nombre que deseas buscar : ")
+        if SearchName in Almacenamiento["nombres"]:
+            NombreEncontrado = Almacenamiento["nombres"].index(SearchName)
+            print(f"El nombre que registraste es {SearchName} y la posición es {NombreEncontrado}")
             return
         else:
-            print("No se encontro el nombre ingresado")
+            print("No se encontro el nombre ingresado \n")
             return
     elif Response == 2:
-        print("Tu elección es busqueda de numeros")
+        print("Tu elección es busqueda de numeros \n")
         SearchNumber = int(input("Introduce el numero que deseas buscar : "))
         if SearchNumber in Almacenamiento["numeros"]:
             NumeroEncontrado = Almacenamiento["numeros"].index(SearchNumber)
-            print(f"El numero que ingresaste es {SearchNumber} y la posición es {NumeroEncontrado}") 
+            print(f"El numero que registraste es {SearchNumber} y la posición es {NumeroEncontrado}") 
             return
         else:
-            print("No se encontro el numero ingresado")
+            print("No se encontro el numero ingresado \n")
             return
     elif Response == 3:
         print("Tu elección es busqueda de direcciones")
-        SearchDir = input("Introduce la dirección que deseas buscar :")
-        if SearchDir in Almacenamiento["direcciones"]:
-            DirecciónEncontrada = Almacenamiento["direcciones"].index(SearchDir)
-            print(f"La dirección que ingresaste es {SearchDir} y la posición es {DirecciónEncontrada}")
+        SearchAdress = input("Introduce la dirección que deseas buscar :")
+        if SearchAdress in Almacenamiento["direcciones"]:
+            DirecciónEncontrada = Almacenamiento["direcciones"].index(SearchAdress)
+            print(f"La dirección que registraste es {SearchAdress} y la posición es {DirecciónEncontrada}")
             return
         else:
             print("No se encontro la dirección ingresada")
@@ -93,6 +92,18 @@ def busqueda():
             return None
         elif AgainMenu2 == "no":
             RetiradaDeEmergencia() 
+
+def showData():
+    print("\n 1. Nombres \n 2. Números \n 3. Direcciones \n 4. Volver al menú principal")
+    option3 = int(input("Ingresa la opción del contenido que quieres ver: "))
+    if option3 == 1:
+        print("Nombres:", Almacenamiento["nombres"])
+    elif option3 == 2:
+        print("Números:", Almacenamiento["numeros"])
+    elif option3 == 3:
+        print("Direcciones:", Almacenamiento["direcciones"])
+    elif option3 == 4:
+        return None
 
 def DelCliente():
     print("¿Qué quieres eliminar?")
@@ -109,13 +120,14 @@ def DelCliente():
             return
     elif response == 2:
         print("Tu elección es eliminación de numeros")
-        DelNumber = input("Introduce el numero que deseas eliminar: ")
+        DelNumber = int(input("Introduce el numero que deseas eliminar: "))
         if DelNumber in Almacenamiento["numeros"]:
             Almacenamiento["numeros"].remove(DelNumber)
             print(f"El número {DelNumber} ha sido eliminado")
             return 
-        else:
-            return "No se encontró el número ingresado"
+        else: 
+            print("No se encontró el número ingresado")
+            return
     elif response == 3:
         print("Tu elección es eliminación de direcciones")
         DelAdress = input("Introduce la dirección que deseas eliminar: ")
@@ -133,48 +145,38 @@ def DelCliente():
         elif AgainMenu == "no":
             return    
 
-def showData():
-    print("\n 1. Nombres \n 2. Números \n 3. Direcciones \n 4. Volver al menú principal")
-    option3 = int(input("Ingresa la opción del contenido que quieres ver: "))
-    if option3 == 1:
-        print("Nombres:", Almacenamiento["nombres"])
-    elif option3 == 2:
-        print("Números:", Almacenamiento["numeros"])
-    elif option3 == 3:
-        print("Direcciones:", Almacenamiento["direcciones"])
-    elif option3 == 4:
-        return None
-
-name = input("\nintroduzca su nombre\n")
+name = input("\nIntroduzca su nombre\n")
 print(f"\nBienvenido querid@ {name}\n")
 
+# Muestra Menu Principal 
 while True :
     MenuPrincipal()
-    Option = (int(input("Introduzca su proximo movimiento : ")))
+    Option = int(input("Introduzca su proximo movimiento : "))
     
+    # Opción 1. Registrar clientes
     if  Option == 1 :
         while True:
              regSave = registro()
              if regSave is None:
                 break
             
-        Decision = (input("\n¿Quieres registrar otro dato?, responde con 'sí' o 'no' : ")).lower()
-        if Decision == "si":
-            MenuPrincipal()
-        elif Decision == "no":
+        DecisionData1 = (input("\n¿Quieres registrar otro dato?, responde con 'sí' o 'no' : ")).lower()
+        if DecisionData1 == "si":
+            registro()
+        elif DecisionData1 == "no":
             RetiradaDeEmergencia()
-            
+    # Opción 2. Buscar clientes
     elif Option == 2:
         while True:
             Search1 = busqueda()
             if Search1 is None:
                  break 
-            Decision = (input("\n¿Quieres Buscar otro dato?, responde con 'sí' o 'no' : ")).lower()
-            if Decision == "si":
+            DecisionData2 = (input("\n¿Quieres Buscar otro dato?, responde con 'sí' o 'no' : ")).lower()
+            if DecisionData2 == "si":
                 busqueda()
-            elif Decision == "no": 
+            elif DecisionData2 == "no": 
                 RetiradaDeEmergencia()
-   
+    # Opción 3. Mostrar lista de clientes
     elif Option == 3:
         while True:
             MostrarDatos = showData()
@@ -184,33 +186,34 @@ while True :
             print("Nombres:", Almacenamiento["nombres"])
             print("Números:", Almacenamiento["numeros"])
             print("Direcciones:", Almacenamiento["direcciones"])
-            decision = input("\n¿Quieres ver otra opción? Responde con 'sí' o 'no': ").lower()
-            if decision == "si":
+            LookOption1 = input("\n¿Quieres ver otra opción? Responde con 'sí' o 'no': ").lower()
+            if LookOption1 == "si":
                 showData()
-            elif decision == "no":
-                Decision = input("\n¿Quieres regresar al menú principal? Responde con 'sí' o 'no': ").lower()
-                if Decision == "si":
+            elif LookOption1 == "no":
+                BackMenu3 = input("\n¿Quieres regresar al menú principal? Responde con 'sí' o 'no': ").lower()
+                if BackMenu3 == "si":
                     MenuPrincipal()
-                elif Decision == "no":
+                elif BackMenu3 == "no":
                     RetiradaDeEmergencia()
-
+    # Opción 4. Eliminar clientes
     elif Option == 4:
         while True: 
             ClienteEliminado = DelCliente()
             if ClienteEliminado is None:
                 break
             print(ClienteEliminado)
-            decision = input("\n¿Quieres ver otra opción? Responde con 'sí' o 'no': ").lower()
-            if decision == "si":
+            LookOption2 = input("\n¿Quieres ver otra opción? Responde con 'sí' o 'no': ").lower()
+            if LookOption2 == "si":
                 DelCliente()
             else:
-                Decision = input("\n¿Quieres regresar al menú principal? Responde con 'sí' o 'no' : ").lower()
-                if Decision == "si":
+                BackMenu4 = input("\n¿Quieres regresar al menú principal? Responde con 'sí' o 'no' : ").lower()
+                if BackMenu4 == "si":
                     MenuPrincipal()
-                elif Decision == "no":
+                elif BackMenu4 == "no":
                     RetiradaDeEmergencia()
+    # Opción 5. Salida del sistema
     elif Option == 5:
-        print("Gracias por recurrir a tu registradora de confianza, ten un lindo día")
+        print("Gracias por recurrir a tu sistema de registro de Clientes de confianza, ten un lindo día")
         break
 
 
